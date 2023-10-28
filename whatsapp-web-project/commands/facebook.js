8f775c9bc0a6f3992d88c6fa61e78fefb70a44c1
abdellaherguig <l72741421@gmail.com>
2023-10-28 17:53:35 +0100

    sds

--- whatsapp-web-project/commands/facebook.js
+++ whatsapp-web-project/commands/facebook.js
@@ -1,5 +1,6 @@
 // facebook.js
 const axios = require('axios');
+const { MessageMedia } = require('whatsapp-web.js');
 
 async function getFacebookInfo(message, args) {
     if (args.length < 2) {
@@ -20,7 +21,17 @@ async function getFacebookInfo(message, args) {
         try {
             const response = await axios.request(options);
             const responseData = response.data;
-            message.reply(`Response: ${JSON.stringify(responseData)}`);
+
+            const links = extractLinks(responseData);
+            if (links) {
+                for (const [key, value] of links) {
+                    // Send media from URL
+                    const media = await MessageMedia.fromUrl(value);
+                    await message.reply(`Key: ${key}, Value: ${value}`, media);
+                }
+            } else {
+                message.reply('Failed to extract links from the Facebook response.');
+            }
         } catch (error) {
             console.error(error);
             message.reply('An error occurred while processing the Facebook video link.');
